14c20d1916aef2740c219bf823951414840436b0
Rustem Melnichenko <ratface@Mac.verizon.net>
2025-06-22 10:35:04 -0400

    added JBDC url

--- server/src/main/kotlin/ai/toolio/app/Config.kt
+++ server/src/main/kotlin/ai/toolio/app/Config.kt
@@ -2,6 +2,6 @@ package ai.toolio.app
 
 object ToolioConfig {
     val rawUrl = System.getenv("DATABASE_URL") ?: error("DATABASE_URL not set")
-    val jdbcUrl = "jdbc:$rawUrl"
+    val jdbcUrl = System.getenv("JDBC_DATABASE_URL") ?: error("JDBC_DATABASE_URL not set")
     val storagePath = System.getenv("STORAGE_PATH") ?: "/app/storage"
 }

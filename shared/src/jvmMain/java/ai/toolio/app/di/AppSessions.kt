7092d2f68eadc47de9934dd48067117cc0fe8caf
Rustem Melnichenko <ratface@Mac.verizon.net>
2025-07-02 13:49:46 -0400

    fix detekt

--- shared/src/jvmMain/java/ai/toolio/app/di/AppSessions.kt
+++ shared/src/jvmMain/java/ai/toolio/app/di/AppSessions.kt
@@ -1,14 +1,16 @@
 package ai.toolio.app.di
 
 actual object AppSessions {
-    actual fun saveUserId() {
+    actual fun saveUserId(id: String) {
+        TODO("Not yet implemented")
     }
 
     actual fun getUserId(): String {
         TODO("Not yet implemented")
     }
 
-    actual fun saveUserNickname(id: String) {
+    actual fun saveUserNickname(nickname: String) {
+        TODO("Not yet implemented")
     }
 
     actual fun getUserNickname(): String {
